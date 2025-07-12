6e5a89a929a49e5b3916ecf30cabde6c44b231d2
Marley Castro <162909728+MarleyCastro@users.noreply.github.com>
2025-07-12 12:25:11 -0300

    main

---
+++ script.js
@@ -0,0 +1,101 @@
+    function onChangeEmail() {
+    toggleButtonsDisable();
+    toggleEmailErrors();
+    }
+
+    function onChangePassword() {
+    toggleButtonsDisable();
+    togglePasswordErrors();
+    }
+
+    function login() {
+    showLoading(); //so irá mostrar a tela de carregamento quando for realirado o login
+    firebase
+        .auth()
+        .signInWithEmailAndPassword(form.email().value, form.password().value)
+        .then(() => {
+        hideLoading();
+        window.location.href = "pages/home/home.html";
+        })
+        .catch((error) => {
+        hideLoading();
+        alert(getErrorMessage(error));
+        });
+    }
+
+    function getErrorMessage(error) {
+
+    if (error.code == "auth/invalid-email"){
+        return "Email inválido";
+    }
+    if (error.code == "auth/user-not-found") {
+        return "Usuário nao encontrado";
+    }
+    if (error.code == "auth/wrong-password") {
+        return "Senha inválida";
+    }
+    if (error.code == "auth/invalid-login-credentials"){
+        return "Senha ou email inválidos";
+    } return error.message;
+    }
+
+    function register() {
+    window.location.href = "pages/register/register.html";
+    }
+
+    function recoverPassword(){
+        showLoading();
+        firebase.auth().sendPasswordResetEmail(form.email().value).then(() => {
+            hideLoading();
+            alert('Email enviado com sucesso');
+        }).catch(error => {
+            hideLoading();
+            alert(getErrorMessage(error));
+        })
+    }
+
+    function toggleEmailErrors() {
+    const email = form.email().value;
+    form.emailRequiredError().style.display = email ? "none" : "block";
+
+    form.emailInvalidError().style.display = validateEmail(email)
+        ? "none"
+        : "block";
+    }
+
+    function togglePasswordErrors() {
+    const password = form.password().value;
+    form.passwordRequiredError().style.display = password ? "none" : "block";
+    }
+
+    function toggleButtonsDisable() {
+    const emailValid = isEmailValid();
+    form.recoverPasswordButton().disabled = !emailValid;
+
+    const passwordValid = isPasswordValid();
+    form.loginButton().disabled = !emailValid || !passwordValid;
+    }
+
+    function isEmailValid() {
+    const email = form.email().value;
+    if (!email) {
+        return false;
+    }
+    return validateEmail(email);
+    }
+
+    function isPasswordValid() {
+    return form.password().value ? true : false;
+    }
+
+    const form = {
+    email: () => document.getElementById("email"),
+    emailInvalidError: () => document.getElementById("email-invalid-error"),
+    emailRequiredError: () => document.getElementById("email-required-error"),
+    loginButton: () => document.getElementById("login-button"),
+    password: () => document.getElementById("password"),
+    passwordRequiredError: () =>
+        document.getElementById("password-required-error"),
+    recoverPasswordButton: () =>
+        document.getElementById("recover-password-button"),
+    };
